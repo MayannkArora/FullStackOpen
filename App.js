22b9257710521618e8a46566617e11f9d7ccc0e8
MayannkArora <101288088+MayannkArora@users.noreply.github.com>
2023-03-16 01:53:20 +0530

    Add app.js and index.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CourseFinal from './Components/CourseFinal';
+
+function App() {
+  return (
+    <div>
+   
+      <CourseFinal />
+    </div>
+  );
+}
+
+export default App;
